034702989d6c92b0dfbb3e50831b2ca54ddc8e08
fabiobaptista <fhbaptista@gmail.com>
2022-05-16 18:27:37 -0300

    fix: fixed eslint scripts in package.json

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,7 +1,7 @@
-import { HttpPostClientSpy } from 'data/test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
+import { HttpPostClientSpy } from '../../../data/test/mock-http-client'
 import faker from '@faker-js/faker'
-import { mockAuthentication } from 'domain/test/mock-authentication'
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -19,18 +19,18 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 describe('Remote Authentication', () => {
   test('Should call httpPostClient with correct URL', async () => {
     const url = faker.internet.url()
-    const { sut, httpPostClientSpy } = makeSut()
+    const { sut, httpPostClientSpy } = makeSut(url)
 
-    await sut.auth()
+    await sut.auth(mockAuthentication())
 
     expect(httpPostClientSpy.url).toBe(url)
   })
 
   test('Should call httpPostClient with correct body', async () => {
     const { sut, httpPostClientSpy } = makeSut()
+    const authParams = mockAuthentication()
+    await sut.auth(authParams)
 
-    await sut.auth(mockAuthentication())
-
-    expect(httpPostClientSpy.url).toBe(url)
+    expect(httpPostClientSpy.body).toEqual(authParams)
   })
 })
